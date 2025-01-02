65675d85caec0effb01ebfa087a866fa0fa5056c
Jose A. Canto <jose.a.c.lopez@gmail.com>
2025-01-02 21:50:04 +0100

    fix hydratation

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,7 +17,7 @@ export default function RootLayout({
   children: React.ReactNode
 }>) {
   return (
-    <html lang="en" className="dark" style={{ colorScheme: "dark" }}>
+    <html lang="en" suppressHydrationWarning>
       <body className={`${montserrat.variable} antialiased`}>
         <AppWrapper>
           {children}
