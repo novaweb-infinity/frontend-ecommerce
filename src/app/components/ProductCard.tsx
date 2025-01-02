73a3217ed42a6eb7b1284be6e509bb41cec2cf2b
Jose A. Canto <jose.a.c.lopez@gmail.com>
2025-01-02 14:25:48 +0100

    remove comments

--- src/app/components/ProductCard.tsx
+++ src/app/components/ProductCard.tsx
@@ -10,12 +10,8 @@ export default function ProductCard({ product }: { product: Product }) {
 
   return (
     <Card className="mx-auto w-full max-w-xs md:max-w-sm lg:max-w-md xl:max-w-lg">
-      {" "}
-      {/* Ajusta el tamaño de la tarjeta */}
       <CardContent className="flex flex-col items-center p-4">
         <div className="flex h-64 w-full items-center justify-center overflow-hidden md:h-72 lg:h-80 xl:h-96">
-          {" "}
-          {/* Contenedor para la imagen */}
           <Image
             src={imageUrl}
             alt={product.productName}
@@ -24,8 +20,7 @@ export default function ProductCard({ product }: { product: Product }) {
             height={300}
             objectFit="cover"
             className="rounded-md"
-          />{" "}
-          {/* Ajusta el tamaño de la imagen */}
+          />
         </div>
         <h3 className="mt-2 text-center text-lg font-semibold">{product.productName}</h3>
         <p className="text-center text-gray-600">${product.price.toFixed(2)}</p>
