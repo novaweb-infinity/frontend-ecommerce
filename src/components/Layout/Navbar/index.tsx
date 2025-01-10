0206b50309c7f7a5712986abad78dbf746f5f62e
Jose A. Canto <jose.a.c.lopez@gmail.com>
2025-01-10 21:40:14 +0100

    refactor: move email retrieval to useEffect and improve Navbar layout

--- src/components/Layout/Navbar/index.tsx
+++ src/components/Layout/Navbar/index.tsx
@@ -1,7 +1,7 @@
 "use client"
 
 import Link from "next/link"
-import { useState } from "react"
+import { useEffect, useState } from "react"
 import { useSelector } from "react-redux"
 
 import { ShoppingSheet } from "@/components/Sheet/ShoppingSheet"
@@ -13,23 +13,27 @@ import ToggleTheme from "./ToggleTheme"
 
 export default function Navbar() {
   const cartItems = useSelector((state: RootState) => state.cart.items)
+  const [email, setEmail] = useState<string | null>(null)
 
-  const tokenString = sessionStorage.getItem("supabase.auth.token")
-  let email = ""
+  useEffect(() => {
+    const tokenString = sessionStorage.getItem("supabase.auth.token")
 
-  if (tokenString) {
-    try {
-      const token = JSON.parse(tokenString)
-      email = token.user.email
-    } catch (error) {
-      console.error("Error parsing token:", error)
+    if (tokenString) {
+      try {
+        const token = JSON.parse(tokenString)
+        setEmail(token.user.email)
+      } catch (error) {
+        console.error("Error parsing token:", error)
+      }
     }
-  }
+  }, [])
 
   return (
     <nav className="fixed z-10 w-full bg-white shadow-md">
       <div className="relative flex items-center justify-between px-4 py-4">
-        <NavLinks />
+        <div className="flex flex-grow">
+          <NavLinks />
+        </div>
 
         <div className="absolute left-1/2 -translate-x-1/2">
           <Link href="/" className="rounded-lg px-4 py-2 text-2xl font-bold text-black hover:bg-gray-200">
@@ -37,8 +41,8 @@ export default function Navbar() {
           </Link>
         </div>
 
-        <div className="flex items-center justify-between gap-2">
-          <p>{email}</p>
+        <div className="flex flex-grow items-center justify-end gap-2">
+          {email && <p>{email}</p>}
           <ShoppingSheet icon="cart" cartItems={cartItems} favoriteItems={cartItems.length} />
           <ShoppingSheet icon="heart" cartItems={cartItems} favoriteItems={cartItems.length} />
           <Login />
