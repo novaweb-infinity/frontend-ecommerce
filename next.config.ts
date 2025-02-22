abac3d283ad9dbcd2f6dd272bcdd1745cdd045a7
Jose A. Cantó (Jack) <jose.a.c.lopez@gmail.com>
2025-02-22 17:34:24 +0100

    update port strapi aws

--- next.config.ts
+++ next.config.ts
@@ -6,7 +6,7 @@ const nextConfig: NextConfig = {
       "localhost",
       "res.cloudinary.com",
       "images.unsplash.com",
-      "ec2-3-122-233-89.eu-central-1.compute.amazonaws.com",
+      "ec2-3-122-233-89.eu-central-1.compute.amazonaws.com:1337",
     ], // Agrega aquí los dominios desde los que cargarás imágenes
   },
   // Otras configuraciones de Next.js
